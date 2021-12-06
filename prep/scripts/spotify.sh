1260db782c78d46af6e27757325821ccf7f5b27a
gbouzon <ga@bouzon.com.br>
2021-12-06 15:37:27 -0500

    updated scripts after testing

--- prep/scripts/spotify.sh
+++ prep/scripts/spotify.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 #installing snaps and starting process
-chmod 755 snap_software.sh
+#make sure snaps are installed -> add to startup
 chmod 755 start_snap_process.sh
-
-./snap_software.sh
 ./start_snap_process.sh
 
 snap run spotify
